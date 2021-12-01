3a529f00563127cf13a3446b591869fcc3762535
Philipp Montesano <philipp.montesano@icloud.com>
2021-12-01 21:10:41 +0100

    Added workflow and sample unit test

---
+++ src/test/java/com/github/sbpb/librarydisclosure/DummyTest.java
@@ -0,0 +1,13 @@
+package com.github.sbpb.librarydisclosure;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class DummyTest {
+
+  @Test
+  void dummyTest() {
+    fail();
+  }
+}
