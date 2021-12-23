eb41c2e5062782c16974f7f5334745168144c508
Philipp Montesano <philipp.montesano@icloud.com>
2021-12-23 23:27:26 +0100

    Simplified auto-configuration

--- src/main/java/com/github/sbpb/librarydisclosure/configuration/autoconfiguration/LibraryDisclosureAutoConfiguration.java
+++ src/main/java/com/github/sbpb/librarydisclosure/configuration/autoconfiguration/LibraryDisclosureAutoConfiguration.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.github.sbpb.librarydisclosure.configuration.property.LibraryDisclosureProperty;
 import com.github.sbpb.librarydisclosure.library.provider.LibraryProvider;
 import com.github.sbpb.librarydisclosure.library.provider.LibraryProviderDefaultImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
@@ -28,26 +28,13 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(LibraryDisclosureProperty.class)
 public class LibraryDisclosureAutoConfiguration {
 
-  /**
-   * Creates a {@link LibraryProvider} using a local {@link XmlMapper}.
-   */
-  @Bean("libraryProvider")
-  @ConditionalOnMissingBean({XmlMapper.class, LibraryProvider.class})
-  public LibraryProvider libraryProviderLocalXmlMapper(LibraryDisclosureProperty libraryDisclosureProperty) {
-    return createLibraryProvider(new XmlMapper(), libraryDisclosureProperty);
-  }
-
-  /**
-   * Creates a {@link LibraryProvider} using a global {@link XmlMapper} bean.
-   */
-  @Bean("libraryProvider")
-  @ConditionalOnBean(XmlMapper.class)
+  @Bean
   @ConditionalOnMissingBean(LibraryProvider.class)
-  public LibraryProvider libraryProviderGlobalXmlMapperBean(XmlMapper xmlMapper, LibraryDisclosureProperty libraryDisclosureProperty) {
-    return createLibraryProvider(xmlMapper, libraryDisclosureProperty);
-  }
+  public LibraryProvider libraryProvider(@Autowired(required = false) XmlMapper xmlMapper, LibraryDisclosureProperty libraryDisclosureProperty) {
+    if (xmlMapper == null) {
+      xmlMapper = new XmlMapper();
+    }
 
-  private LibraryProvider createLibraryProvider(XmlMapper xmlMapper, LibraryDisclosureProperty libraryDisclosureProperty) {
     return new LibraryProviderDefaultImpl(xmlMapper, libraryDisclosureProperty.getLocation());
   }
 
