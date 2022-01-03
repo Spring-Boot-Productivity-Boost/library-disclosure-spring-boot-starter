f72abd1ba976b7ec94ff4604506967ad296f62cb
Philipp Montesano <philipp.montesano@icloud.com>
2022-01-03 21:48:23 +0100

    Added actuator endpoint to expose library disclosure information

--- src/main/java/com/github/sbpb/librarydisclosure/configuration/autoconfiguration/LibraryDisclosureAutoConfiguration.java
+++ src/main/java/com/github/sbpb/librarydisclosure/configuration/autoconfiguration/LibraryDisclosureAutoConfiguration.java
@@ -1,11 +1,14 @@
 package com.github.sbpb.librarydisclosure.configuration.autoconfiguration;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.github.sbpb.librarydisclosure.actuator.endpoint.LibraryDisclosureEndpoint;
 import com.github.sbpb.librarydisclosure.configuration.property.LibraryDisclosureProperty;
 import com.github.sbpb.librarydisclosure.library.provider.LibraryProvider;
 import com.github.sbpb.librarydisclosure.library.provider.LibraryProviderDefaultImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -18,9 +21,17 @@ import org.springframework.context.annotation.Configuration;
  * Auto-configuration for library disclosure.
  *
  * <ul>
- *   <li>Active unless deactivated with the feature flag.</li>
- *   <li>Makes use of the {@link LibraryDisclosureProperty} configuration.</li>
+ *   <li>Active unless deactivated with the "sbpb.librarydisclosure.enabled" feature flag.</li>
+ *   <li>Makes use of the {@link LibraryDisclosureProperty} configuration properties.</li>
  *   <li>When a global {@link XmlMapper} bean is available, this one is used.</li>
+ * </ul>
+ *
+ * Exposes the library disclosure information via an actuator endpoint.
+ *
+ * <ul>
+ *   <li>If the required actuator dependencies are available.</li>
+ *   <li>If a {@link LibraryProvider} bean is available.</li>
+ *   <li>If not deactivated with the "sbpb.librarydisclosure.actuator.enabled" feature flag.</li>
  * </ul>
  */
 @Configuration
@@ -40,6 +51,15 @@ public class LibraryDisclosureAutoConfiguration {
     }
 
     return new LibraryProviderDefaultImpl(xmlMapper, libraryDisclosureProperty.getLocation());
+  }
+
+  @Bean
+  @SuppressWarnings("unused")
+  @ConditionalOnClass(Endpoint.class)
+  @ConditionalOnBean(LibraryProvider.class)
+  @ConditionalOnProperty(prefix = "sbpb.librarydisclosure.actuator", name = "enabled", havingValue = "true", matchIfMissing = true)
+  public LibraryDisclosureEndpoint libraryDisclosureEndpoint(LibraryProvider libraryProvider) {
+    return new LibraryDisclosureEndpoint(libraryProvider);
   }
 
 }
